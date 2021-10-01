5e0632f0c12ead0cb0685e287f529fcad8e1ec2a
Diego <learsi_to@live.com.mx>
2021-10-01 16:10:40 -0600

    Primeras prácticas en clase

---
+++ clase1.py
@@ -0,0 +1,25 @@
+print("Hola mundo") #Primer Hola mundo, así de simple.
+#Python es sensible a la identación (Espacios y demás)
+
+#Variables: en python se elige el tipo de variable según el valor que le des a ella. Sólo es necesario poner el nombre de la variable.
+
+#Ligas:
+
+#Condicionales:
+
+if b>4 :
+    otra = 3*"Hola mundos"
+    print(otra)
+else:
+    print("Entré al else")
+    if otra!=3 :
+        print("Estoy en un if contenido en un else")
+#switch no existe en python. El else if se escribe 'elif'
+
+#Funciones: Palabra reservada 'def'
+
+#Parámetros
+
+#Para un input es:
+a = input("Ingresa el valor de a") #Un input va a regresar una cadena siempre. Para cambiar a entero:
+a = int(a)
---
+++ practica1.py
@@ -0,0 +1,22 @@
+#Recibir dos valores. El primero "Cual es el umbral?" El segundo "Cual es el numero a probar?"
+#Resultado "El numero ingresado es aprobado" > umbral
+#"El numero ingresado es reprobado" < umbral
+
+
+umbral=input("Cuál es el umbral?")
+b=input("Cuál es el número a probar?")
+
+if umbral<b:
+    print("El numero ingresado es aprobado")
+elif umbral>b:
+    print("El numero ingresado es reprobado")
+
+x=1+2*3-4/5**6
+print(x)
+
+hola = input("Ya")
+
+#type(variable) para preguntar de qué tipo es mi variable.
+
+#Tarea: realizar un menú para elegir la operación a realizar: + - / *
+#Puedo sumar de 2 a 4 valores
