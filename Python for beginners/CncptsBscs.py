ead54769dbe4e24ed523dd2c88cbf2ca4e0b0076
Diego <ogeid_to@hotmail.com>
2021-10-06 21:31:12 -0600

    Lecciones con python y función errónea en la tarea

---
+++ Python for beginners/CncptsBscs.py
@@ -0,0 +1,27 @@
+#Hello world:
+print('Hello world!')
+#Puedo generar tantos prints como quiera, y cada declaración print generará el texto en una línea nueva:
+print('Hola mundo!')
+print('Jai!')
+print('Arigato!')
+
+#Operaciones simples:
+print(2+2)
+print(5 + 4 - 3)
+print( 10/2 * ( 2*(3+4) ) )
+print((4+8)/2) #Una división (/) generará siempre un float, por lo tanto en este caso el resultado no sería 6, sino 6.0
+
+#Tipos de datos:
+cadena="Esto es una cadena de carácteres"
+entero=5
+flotante=5.5
+print(type(cadena),type(entero),type(flotante))
+#Maneras en que se produce un float:
+print( 8 / 2 )
+print( 6 * 7.0 )
+print( 4 + 1.65 )
+
+#Exponenciación:
+
+
+a=input("Listo!")
--- Tarea1 - Calculadora.py
+++ Tarea1 - Calculadora.py
@@ -1,7 +1,15 @@
-a=input("Numero ")
-b=input("Numero dos: ")
-a=int(a)
-int(b)
-respuesta=a+b
-print("La respuesta es: " respuesta)
+#spam = input("Cuántas cifras desea operar? (Elija de 2 a 4): ")
+
+def dosvar():
+    a=input("Introduzca el valor: ")
+    op=input("Elija una operación (+, -, /, *): ")
+    b=input("Introduzca el valor: ")
+    print("\nEl resultado es: ")
+    if op == "+":
+
+        print( int(a) + int(b) )
+    else: 
+        print("ERROR EN EL OPERADOR")
+
+dosvar()
 z=input("Es correcto? ")
