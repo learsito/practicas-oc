2a92628103b8c635f9ce2ab5222d0bcb649daef7
Diego <ogeid_to@hotmail.com>
2021-10-07 16:56:39 -0600

    Tarea terminada

--- Tarea1 - Calculadora.py
+++ Tarea1 - Calculadora.py
@@ -1,15 +1,226 @@
-#spam = input("Cuántas cifras desea operar? (Elija de 2 a 4): ")
+error="ERROR EN EL OPERADOR"
 
 def dosvar():
-    a=input("Introduzca el valor: ")
-    op=input("Elija una operación (+, -, /, *): ")
-    b=input("Introduzca el valor: ")
+    a=input("Introduzca el primer valor: ")
+    op=input("Elija operador (+,  -,  /,  *): ")
+    b=input("Introduzca siguiente valor: ")
     print("\nEl resultado es: ")
+    a=int(a)
+    b=int(b)
     if op == "+":
-
-        print( int(a) + int(b) )
+        print( a + b )
+    elif op == '-':
+        print(a - b)
+    elif op == '/':
+        print(a / b)
+    elif op == '*':
+        print(a * b)
     else: 
-        print("ERROR EN EL OPERADOR")
+        print(error)
 
-dosvar()
-z=input("Es correcto? ")
+def tresvar():
+    a=input("Introduzca el primer valor: ")
+    op=input("Elija operador (+, -, /, *): ")
+    b=input("Introduzca siguiente valor: ")
+    op2=input("Introduzca siguiente operador (+, -, /, *): ")
+    c=input("Introduzca siguiente valor: ")
+    print('El resultado es: ')
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    if op == '+' and op2 == '+':
+        print( a + b + c)
+    elif op == '+' and op2 == '-':
+        print(a + b - c)
+    elif op == '+' and op2 == '/':
+        print(a + b / c)
+    elif op == '+' and op2 == '*':
+        print(a + b * c)
+    elif op == '-' and op2 == '+':
+        print(a - b + c)
+    elif op == '-' and op2 == '-':
+        print(a - b - c)
+    elif op == '-' and op2 == '/':
+        print(a - b / c)
+    elif op == '-' and op2 == '*':
+        print(a - b * c)
+    elif op == '/' and op2 == '+':
+        print(a / b + c)
+    elif op == '/' and op2 == '-':
+        print(a / b - c)
+    elif op == '/' and op2 == '/':
+        print(a / b / c)
+    elif op == '/' and op2 == '*':
+        print(a / b * c)
+    elif op == '*' and op2 == '+':
+        print(a * b + c)
+    elif op == '*' and op2 == '-':
+        print(a * b - c)
+    elif op == '*' and op2 == '/':
+        print(a * b / c)
+    elif op == '*' and op2 == '*':
+        print(a * b * c)
+    else:
+        print(error)
+
+def quadvar():
+    a=input("Introduzca el primer valor: ")
+    op=input("Elija operador (+, -, /, *): ")
+    b=input("Introduzca siguiente valor: ")
+    op2=input("Introduzca siguiente operador (+, -, /, *): ")
+    c=input("Introduzca siguiente valor: ")
+    op3=input("Introduzca siguiente operador (+, -, /, *): ")
+    d=input("Introduzca siguiente valor: ")
+    print('El resultado es: ')
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    d=int(d)
+    if op == '+' and op2 == '+' and op3 == '+':
+        print(a + b + c + d)
+    elif op == '+' and op2 == '+' and op3 == '-':
+        print(a + b + c - d)
+    elif op == '+' and op2 == '+' and op3 == '/':
+        print(a + b + c / d)
+    elif op == '+' and op2 == '+' and op3 == '*':
+        print(a + b + c * d)
+    elif op == '+' and op2 == '-' and op3 == '+':
+        print(a + b - c + d)
+    elif op == '+' and op2 == '-' and op3 == '-':
+        print(a + b - c - d)
+    elif op == '+' and op2 == '-' and op3 == '/':
+        print(a + b - c / d)
+    elif op == '+' and op2 == '-' and op3 == '*':
+        print(a + b - c * d)
+    elif op == '+' and op2 == '/' and op3 == '+':
+        print(a + b / c + d)
+    elif op == '+' and op2 == '/' and op3 == '-':
+        print(a + b / c - d)
+    elif op == '+' and op2 == '/' and op3 == '/':
+        print(a + b / c / d)
+    elif op == '+' and op2 == '/' and op3 == '*':
+        print(a + b / c * d)
+    elif op == '+' and op2 == '*' and op3 == '+':
+        print(a + b * c + d)
+    elif op == '+' and op2 == '*' and op3 == '-':
+        print(a + b * c - d)
+    elif op == '+' and op2 == '*' and op3 == '/':
+        print(a + b * c / d)
+    elif op == '+' and op2 == '*' and op3 == '*':
+        print(a + b * c * d)
+    elif op == '-' and op2 == '+' and op3 == '+':
+        print(a - b + c + d)
+    elif op == '-' and op2 == '+' and op3 == '-':
+        print(a - b + c - d)
+    elif op == '-' and op2 == '+' and op3 == '/':
+        print(a - b + c / d)
+    elif op == '-' and op2 == '+' and op3 == '*':
+        print(a - b + c * d)
+    elif op == '-' and op2 == '-' and op3 == '+':
+        print(a - b - c + d)
+    elif op == '-' and op2 == '-' and op3 == '-':
+        print(a - b - c - d)
+    elif op == '-' and op2 == '-' and op3 == '/':
+        print(a - b - c / d)
+    elif op == '-' and op2 == '-' and op3 == '*':
+        print(a - b - c * d)
+    elif op == '-' and op2 == '/' and op3 == '+':
+        print(a - b / c + d)
+    elif op == '-' and op2 == '/' and op3 == '-':
+        print(a - b / c - d)
+    elif op == '-' and op2 == '/' and op3 == '/':
+        print(a - b / c / d)
+    elif op == '-' and op2 == '/' and op3 == '*':
+        print(a - b / c * d)
+    elif op == '-' and op2 == '*' and op3 == '+':
+        print(a - b * c + d)
+    elif op == '-' and op2 == '*' and op3 == '-':
+        print(a - b * c - d)
+    elif op == '-' and op2 == '*' and op3 == '/':
+        print(a - b * c / d)
+    elif op == '-' and op2 == '*' and op3 == '*':
+        print(a - b * c * d)
+    elif op == '/' and op2 == '+' and op3 == '+':
+        print(a / b + c + d)
+    elif op == '/' and op2 == '+' and op3 == '-':
+        print(a / b + c - d)
+    elif op == '/' and op2 == '+' and op3 == '/':
+        print(a / b + c / d)
+    elif op == '/' and op2 == '+' and op3 == '*':
+        print(a / b + c * d)
+    elif op == '/' and op2 == '-' and op3 == '+':
+        print(a / b - c + d)
+    elif op == '/' and op2 == '-' and op3 == '-':
+        print(a / b - c - d)
+    elif op == '/' and op2 == '-' and op3 == '/':
+        print(a / b - c / d)
+    elif op == '/' and op2 == '-' and op3 == '*':
+        print(a / b - c * d)
+    elif op == '/' and op2 == '/' and op3 == '+':
+        print(a / b / c + d)
+    elif op == '/' and op2 == '/' and op3 == '-':
+        print(a / b / c - d)
+    elif op == '/' and op2 == '/' and op3 == '/':
+        print(a / b / c / d)
+    elif op == '/' and op2 == '/' and op3 == '*':
+        print(a / b / c * d)
+    elif op == '/' and op2 == '*' and op3 == '+':
+        print(a / b * c + d)
+    elif op == '/' and op2 == '*' and op3 == '-':
+        print(a / b * c - d)
+    elif op == '/' and op2 == '*' and op3 == '/':
+        print(a / b * c / d)
+    elif op == '/' and op2 == '*' and op3 == '*':
+        print(a / b * c * d)
+    elif op == '*' and op2 == '+' and op3 == '+':
+        print(a * b + c + d)
+    elif op == '*' and op2 == '+' and op3 == '-':
+        print(a * b + c - d)
+    elif op == '*' and op2 == '+' and op3 == '/':
+        print(a * b + c / d)
+    elif op == '*' and op2 == '+' and op3 == '*':
+        print(a * b + c * d)
+    elif op == '*' and op2 == '-' and op3 == '+':
+        print(a * b - c + d)
+    elif op == '*' and op2 == '-' and op3 == '-':
+        print(a * b - c - d)
+    elif op == '*' and op2 == '-' and op3 == '/':
+        print(a * b - c / d)
+    elif op == '*' and op2 == '-' and op3 == '*':
+        print(a * b - c * d)
+    elif op == '*' and op2 == '/' and op3 == '+':
+        print(a * b / c + d)
+    elif op == '*' and op2 == '/' and op3 == '-':
+        print(a * b / c - d)
+    elif op == '*' and op2 == '/' and op3 == '/':
+        print(a * b / c / d)
+    elif op == '*' and op2 == '/' and op3 == '*':
+        print(a * b / c * d)
+    elif op == '*' and op2 == '*' and op3 == '+':
+        print(a * b * c + d)
+    elif op == '*' and op2 == '*' and op3 == '-':
+        print(a * b * c - d)
+    elif op == '*' and op2 == '*' and op3 == '/':
+        print(a * b * c / d)
+    elif op == '*' and op2 == '*' and op3 == '*':
+        print(a * b * c * d)
+    else:
+        print(error)
+
+loop ='no'
+print("~~~~~~~~~~BIENVENIDO~~~~~~~~~~")
+while loop  == 'no':
+    print("Elija una opción: ")
+    spam = input("Cuántas cifras desea operar? (Elija de 2 a 4): ")
+    if spam == '2':
+        dosvar()
+    elif spam == '3':
+        tresvar()
+    elif spam == '4':
+        quadvar()
+    else:
+        print("No es posible operar con esta cantidad de sifras.")
+    loop=input('Desea salir del programa? (Escriba "si" o "no"): ')
+    if loop != 'si' and loop != 'no':
+        print("Esta opción no es posible. Regresando al menú principal.")
+        loop='no'
