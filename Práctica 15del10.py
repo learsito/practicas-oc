84b8b81e556ec8fde81962fce70308da86ebe381
Diego <ogeid_to@hotmail.com>
2021-10-15 16:01:40 -0600

    Pruebas con juegos

---
+++ Práctica 15del10.py
@@ -0,0 +1,8 @@
+listaUno = ["Halo", "DarkSouls","CS:GO","RainbowSix"  ]
+listaDos = [ 1500,700,"Free",1800 ]
+
+listaTres = [ [listaUno[0], listaDos[0]], [ listaUno[1],listaDos[1] ], [listaUno[2],listaDos[2] ], [ listaUno[3],listaDos[3] ] ]
+
+def spam():
+    for i:
+        print(listaTres[i][0])
